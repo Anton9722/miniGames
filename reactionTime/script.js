52e2443bf150c6578b3590a5842253671e1f4f23
Anton Johansson <anton9797johansson@hotmail.com>
2023-10-26 13:29:36 +0200

    done with reaction time game

--- reactionTime/script.js
+++ reactionTime/script.js
@@ -2,34 +2,47 @@ let reactTimeDiv = document.getElementById("divToClickForReactionTime");
 let reactionTimeImage = document.getElementById("reactionTimeImage");
 let textInReactionTime = document.getElementById("textInReactionTime");
 
-reactTimeDiv.addEventListener("click", function () {
-  let btn = document.createElement("button");
-  btn.id = "reactionBtn";
-  btn.innerHTML = "CLICK ME WHEN SCREEN GOES GREEN!";
-
-  reactTimeDiv.appendChild(btn);
-  reactTimeDiv.style.backgroundColor = "red";
-  reactionTimeImage.remove();
-  textInReactionTime.remove();
-  //creates random number between 1-10
-  let random = Math.floor(Math.random() * 10) + 1;
-  console.log("random antal sekunder " + random);
-  //waits for random amount of seconds and then turn div green
-  setTimeout(
-    () => (reactTimeDiv.style.backgroundColor = "green"),
-    random * 1000
-  );
-  let startTime = new Date();
-  btn.addEventListener("click", () => {
-    let stopTime = new Date();
-    let result = Math.abs(startTime.getTime() - stopTime.getTime()) / 1000;
-    result = result - random;
-    console.log("Resultat " + result);
-  });
-});
-
 //gets amount of miliseconds between the two dates
 //let x = new Date();
 //let y = new Date();
 //let timeelapsed = x - y;
 //console.log(timeelapsed);
+
+let functionNotRunning = true;
+function runFor10Seconds() {
+  //if its already running dont run again, insteed go to else
+  if (functionNotRunning) {
+    reactTimeDiv.style.backgroundColor = "#190482";
+    console.log("Running");
+    reactionTimeImage.remove();
+    textInReactionTime.innerHTML = "Wait For Red...";
+    functionNotRunning = false;
+    //Create random number between 10000 - 1000, we use this to get random amout of miliseconds
+    let randomTime = Math.floor(Math.random() * 10000) + 1000;
+    console.log(randomTime);
+    setTimeout(function () {
+      reactTimeDiv.style.backgroundColor = "red";
+      textInReactionTime.innerHTML = "Now!!!!";
+      //get start time
+      let startTime = new Date();
+      reactTimeDiv.addEventListener("click", function timeTaker() {
+        //gets time when user click and give reaction time result to user on screen
+        let stopTime = new Date();
+        let timeElapsed = stopTime - startTime;
+        timeElapsed = timeElapsed / 1000;
+        reactTimeDiv.style.backgroundColor = "green";
+        textInReactionTime.innerHTML =
+          "Reaction Time: " + timeElapsed + " Sec (Click here to play again)";
+        //remove event listner to not update number score after every click after
+        reactTimeDiv.removeEventListener("click", timeTaker);
+      });
+      //without this the game start again when user click to get result first time
+      reactTimeDiv.addEventListener("click", function dontStartAgainRightNow() {
+        functionNotRunning = true;
+        reactTimeDiv.removeEventListener("click", dontStartAgainRightNow);
+      });
+    }, randomTime);
+  }
+}
+
+reactTimeDiv.addEventListener("click", runFor10Seconds);
